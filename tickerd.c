a506c13177a9a12f425273f7d0093ee6dcdf1734
James Turner <james@calminferno.net>
2014-12-26 18:58:57 -0500

    Replace bitfinex.com with blockchain.info

--- tickerd.c
+++ tickerd.c
@@ -35,7 +35,7 @@ static void
 update_ticker(char *host, char *path)
 {
 	char *sub = NULL, *port = "443";
-	char buf[BUFSIZ];
+	char buf[4096];
 	struct tls_config *config;
 	struct tls *ctx;
 	size_t len;
@@ -68,9 +68,9 @@ update_ticker(char *host, char *path)
 		ticker = json_value_get_object(root);
 
 		if (json_value_get_type(json_object_get_value(ticker,
-			"last_price")) == JSONString) {
-			price = price + atof(json_object_get_string(ticker,
-				"last_price"));
+			"USD")) == JSONObject) {
+			price = price + json_object_dotget_number(ticker,
+				"USD.last");
 			cnt++;
 		} else {
 			price = price + json_object_dotget_number(ticker,
@@ -135,7 +135,7 @@ main(int argc, char *argv[])
 
 	while (1) {
 		update_ticker("btc-e.com", "/api/2/btc_usd/ticker");
-		update_ticker("api.bitfinex.com", "/v1/ticker/btcusd");
+		update_ticker("blockchain.info", "/ticker");
 		write_price(fp);
 		sleep(30);
 	}
